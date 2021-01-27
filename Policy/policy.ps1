9fd2c01c6ef3bfc4d7bbbfedc655e4147bc4d2fd
Lars Sørensen <lso@proactive.dk>
2021-01-27 10:07:54 +0100

    test

--- Policy/policy.ps1
+++ Policy/policy.ps1
@@ -2,9 +2,16 @@ $subscriptionId = 'c6482e07-d710-4ba6-b9ee-ac4b235294b4'
 Select-AzSubscription -Subscription $subscriptionId
 
 New-AzDeployment  `
-    -TemplateFile .\azuredeploy.initiative.json `
-   # -TemplateParameterFile ".\Landing%20Zone%20baseline\Microsoft.Authorization\policySetDefinitions\azuredeploy.parameters.json"
+    -TemplateFile .\azuredeploy.initiative.json -Location "westeurope" `
+    -TemplateParameterFile .\AllowedLocations.json
 
 
-   New-AzDeployment  `
-    -TemplateFile .\azuredeploy.policyassigment.json
+New-AzDeployment  `
+    -Location "West Europe" `
+    -TemplateFile .\azuredeploy.policyassigment.json `
+  #  -TemplateParameterFile .\AllowedLocations.json
+
+    
+$Subscription = Get-AzSubscription -SubscriptionName 'MSDN Platforms'
+$PolicySet = Get-AzPolicySetDefinition -Custom | Where-Object { $_.Properties.DisplayName -eq 'Azure Platform Policy Initiative MVP' }
+New-AzPolicyAssignment -Name 'MyInitiative' -PolicySetDefinition $PolicySet -Scope "/subscriptions/$($Subscription.Id)" -AssignIdentity -Location 'westeurope' -PolicyParameter .\AllowedLocations.json
