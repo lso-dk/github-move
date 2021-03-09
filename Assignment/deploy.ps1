126a647b10309c23eeb5389a1edf979a1d5a7887
Lars Sørensen <37363338+lso-dk@users.noreply.github.com>
2021-03-09 19:51:19 +0100

    added sftp

--- Assignment/deploy.ps1
+++ Assignment/deploy.ps1
@@ -1,6 +1,10 @@
 
 # deploy web app with private endpoints(hostname webapp.cloudtest.dk), app service plan, AppGW with WAF #
-New-AzResourceGroupDeployment -ResourceGroupName MyResGroup -TemplateFile .\deploy.nodejs.json
+New-AzResourceGroupDeployment -name webapp -ResourceGroupName MyResGroup -TemplateFile .\deploy.nodejs.json
 
 # Deploy Azure Cosmos DB Account #
 New-AzResourceGroupDeployment -name mongodb -ResourceGroupName myresgroup -TemplateFile .\deploy.mongodb.json -TemplateParameterFile .\deploy.mongodb.parameters.json
+
+# #
+New-AzResourceGroupDeployment -name sftp -ResourceGroupName myresgroup -TemplateFile .\deploy.sftp.json -TemplateParameterFile .\deploy.sftp.parameters.json
+
